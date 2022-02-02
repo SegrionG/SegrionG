061c09edd44cfd78acf828964579ad999710074f
geraldes-bernardino <adrien.geraldes.bernardino@etu.univ-st-etienne.fr>
2022-02-02 17:53:55 +0100

    Ajout tables de la database et push des tables et FK

--- src/Controller/AdrienController.php
+++ src/Controller/AdrienController.php
@@ -6,6 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterfaces;
+
 
 class AdrienController extends AbstractController
 {
@@ -22,17 +24,21 @@ class AdrienController extends AbstractController
     /**
      * @Route("/formget", name="formget")
      */
-    public function formget(Request $request): Response
+    public function formget(Request $request, EntityManagerInterface $manager): Response
     {
         $username = $request -> request -> get("username") ;
         $password = $request -> request -> get("password") ;
         if( ($username == "root") && ($password == "toor") ) {
             $msg = 'Le mdp et le user sont correctes';
         }
+        elseif($username == "admin") {
+            $msg = 'Vous êtes identifié en tant que admin';
+        }
         else {
             $msg = 'Le mdp et le user ne sont pas correctes';
         }
         return $this->render('adrien/formget.html.twig', [
             'controller_name' => 'AdrienController','login' => $username, 'mdp' => $password , 'message' => $msg]);  
+        
     }
 }
