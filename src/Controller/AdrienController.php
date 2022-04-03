f8ce1803204228823db416e99e731bd8f9e956b7
geraldes-bernardino <adrien.geraldes.bernardino@etu.univ-st-etienne.fr>
2022-04-03 14:40:09 +0200

    derniere modif

--- src/Controller/AdrienController.php
+++ src/Controller/AdrienController.php
@@ -8,6 +8,9 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Utilisateur;
+use App\Entity\Acces;
+use App\Entity\Document;
+use App\Entity\Autorisation;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class AdrienController extends AbstractController
@@ -18,8 +21,9 @@ class AdrienController extends AbstractController
     public function index(Request $request, EntityManagerInterface $manager, SessionInterface $session): Response
     {
         $profidd = $session -> get('numero');
+        $vuser = $session -> get('user');
         return $this->render('adrien/index.html.twig', [
-            'controller_name' => 'AdrienController','profidd' => $profidd
+            'controller_name' => 'AdrienController','profidd' => $profidd,'vuser' => $vuser
         ]);
     }
 
@@ -54,6 +58,7 @@ class AdrienController extends AbstractController
         $username = $request -> request -> get("username") ;
         $password = $request -> request -> get("password") ;
         $usernameV = $manager -> getRepository(Utilisateur::class) -> findOneBy([ "Login" => $username]);
+        $hash = $usernameV -> getPassword();
 
         if ($usernameV==NULL){
             $msg = "Utilisateur inconnu";
@@ -61,7 +66,7 @@ class AdrienController extends AbstractController
             $vuser= "-1";
             $vmdp= "-1";
         }
-        elseif ($password == $usernameV -> getPassword() ){
+        elseif (password_verify($password,$hash)){
             $msg = 'Le mdp et le user sont correctes';
             $profid = $usernameV->getId();
             $session -> set('numero', $profid);
@@ -104,8 +109,9 @@ class AdrienController extends AbstractController
     public function makelogin(Request $request, EntityManagerInterface $manager, SessionInterface $session): Response
     {
         $profidd = $session -> get('numero');
+        $vuser = $session -> get('user');
         return $this->render('adrien/makelogin.html.twig', [
-            'controller_name' => 'AdrienController', 'profidd' => $profidd
+            'controller_name' => 'AdrienController', 'profidd' => $profidd,'vuser' => $vuser
         ]);
     }
         /**
@@ -114,6 +120,7 @@ class AdrienController extends AbstractController
     public function clogin(Request $request, EntityManagerInterface $manager, SessionInterface $session): Response
     {
         $profidd = $session -> get('numero');
+        $vuser = $session -> get('user');
         $cusername = $request -> request -> get("Cusername") ;
         $cpassword = $request -> request -> get("Cpassword") ;
         if (strcmp ($cpassword,'')==0)
@@ -121,6 +128,8 @@ class AdrienController extends AbstractController
         elseif (strcmp ($cusername,'')==0)
             $nusername=NULL;
         else {
+            $nonhashpass = $cpassword;
+            $cpassword = (password_hash($cpassword, PASSWORD_DEFAULT));
             $nusername = new Utilisateur();
             $nusername->setLogin($cusername);
             $nusername->setPassword($cpassword);
@@ -133,10 +142,27 @@ class AdrienController extends AbstractController
         }
         else{
             $message="Le compte a bien été crée.";
+            $nautorisation = new Autorisation();
+            if($nusername=='admin') {
+                $bool1 = 1;
+                $bool2 = 1;
+                $nautorisation->setLecture($bool1);
+                $nautorisation->setEcriture($bool2);
+                $manager->persist($nautorisation);
+                $manager->flush();
+            }
+            else{
+                $bool1 = 1;
+                $bool2 = 0;
+                $nautorisation->setLecture($bool1);
+                $nautorisation->setEcriture($bool2);
+                $manager->persist($nautorisation);
+                $manager->flush();
+            }
         }
 
         return $this->render('adrien/clogin.html.twig', [
-            'controller_name' => 'AdrienController','login' => $cusername, 'mdp' => $cpassword, 'message' => $message,'profidd' => $profidd]);
+            'controller_name' => 'AdrienController','login' => $cusername, 'mdp' => $cpassword, 'message' => $message,'profidd' => $profidd,'vuser' => $vuser]);
     }
     /**
      * @Route("/listelogin", name="listelogin")
@@ -144,9 +170,10 @@ class AdrienController extends AbstractController
     public function listelogin(Request $request, EntityManagerInterface $manager, SessionInterface $session): Response
     {
         $profidd = $session -> get('numero');
+        $vuser = $session -> get('user');
         $listelogin = $manager->getRepository(Utilisateur::class)->findAll();
         return $this->render('adrien/listelogin.html.twig',[
-            'listelogin' => $listelogin,'profidd' => $profidd ]); 
+            'listelogin' => $listelogin,'profidd' => $profidd,'vuser' => $vuser ]); 
     }
         /**
      * @Route("/file2", name="file2")
@@ -154,27 +181,48 @@ class AdrienController extends AbstractController
     public function file2(Request $request, EntityManagerInterface $manager, SessionInterface $session): Response
     {
         $profidd = $session -> get('numero');
+        $vuser = $session -> get('user');
         $path = "/home/etudrt/M4207CservgeraldesD2/public/file21";
         if(file_exists($path)) {}
         else{
         mkdir("file21");
         }
         return $this->render('adrien/file2.html.twig',[
-            'profidd' => $profidd, $_FILES, $path]); 
+            'profidd' => $profidd, $_FILES, $path,'vuser' => $vuser]); 
     }
             /**
      * @Route("/recupfile", name="recupfile")
      */
     public function recupfile(Request $request, EntityManagerInterface $manager, SessionInterface $session): Response
     {
+        $vuser = $session -> get('user');
+        $date = date("y-m-d");  
+        $bool = 1;
         $path = "/home/etudrt/M4207CservgeraldesD2/public/".basename($_FILES['file1']['name']);
         $profidd = $session -> get('numero');
         $_FILES['file1']['tmp_name'];
         $_FILES['file1']['name'];
         basename($_FILES['file1']['name']);
         move_uploaded_file($_FILES['file1']['tmp_name'],$path);
+        $nfile = new Document();
+        $nfile->setChemin($path);
+        $nfile->setDate(new \DateTime($date));
+        $nfile->setActif($bool);
+        $manager->persist($nfile);
+        $manager->flush();
         return $this->render('adrien/recupfile.html.twig',[
-            'profidd' => $profidd, $_FILES]); 
+            'profidd' => $profidd, $_FILES,'vuser' => $vuser]); 
     }
+    /**
+     * @Route("/listefichier", name="listefichier")
+     */
+    public function listefichier(Request $request, EntityManagerInterface $manager, SessionInterface $session): Response
+    {
+        $vuser = $session -> get('user');
+        $profidd = $session -> get('numero');
+        $listefichier = $manager->getRepository(Document::class)->findAll();
 
+        return $this->render('adrien/listefichier.html.twig',[
+            'listefichier' => $listefichier,'profidd' => $profidd,'vuser' => $vuser ]); 
+    }
 }
